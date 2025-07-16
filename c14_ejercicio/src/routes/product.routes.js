15136c3555b537a774cda9e3dc3643d26edb97af
Jose Luis Gonzalez <puchosoft@gmail.com>
2025-07-16 18:37:41 -0300

    Ejercicio 14

---
+++ c14_ejercicio/src/routes/product.routes.js
@@ -0,0 +1,13 @@
+// product.routes.js
+
+import { Router } from 'express';
+import productController from '../controllers/product.controller.js';
+
+const router = Router();
+
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.post('/', productController.addProduct);
+router.delete('/:id', productController.deleteProduct);
+
+export default router;
---
+++ c14_ejercicio/src/utils/folder.js
@@ -0,0 +1,5 @@
+import {join} from 'path';
+
+const __dirname = join(import.meta.dirname,'..');
+
+export { __dirname, join };
